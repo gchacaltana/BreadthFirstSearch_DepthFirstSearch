2d67f3056370f88f5a708f66f36a38521cb8727d
Gonzalo Chacaltana <gchacaltanab@gmail.com>
2019-10-04 23:41:17 -0500

    add nodes to travelers routes

--- App.py
+++ App.py
@@ -8,13 +8,24 @@ __email__ = "gchacaltanab@gmail.com"
 from Node import Node
 
 class App(object):
-    nodes = []
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    nodes = {}
+    def __init__(self):
         self.initNodes()
 
     def initNodes(self):
-        self.nodes = ["Tumbes", "Trujillo", "Moyobamba", "Iquitos"]
+        self.nodes["Tumbes"] = ["Trujillo", "Moyobamba", "Iquitos"]
+        self.nodes["Trujillo"] = ["Lima", "Huancayo"]
+        self.nodes["Lima"] = ["Ica", "Ayacucho"]
+        self.nodes["Ica"] = ["Arequipa"]
+        self.nodes["Arequipa"] = ["Moquegua"]
+        self.nodes["Moyobamba"] = ["Huancayo"]
+        self.nodes["Huancayo"] = ["Ayacucho", "Cusco"]
+        self.nodes["Ayacucho"] = ["Abancay", "Ica"]
+        self.nodes["Cusco"] = ["Puno", "Arequipa"]
+        self.nodes["Puno"] = ["Moquegua"]
+        self.nodes["Moquegua"] = ["Tacna"]
+        self.nodes["Iquitos"] = ["Pucallpa"]
+        self.nodes["Pucallpa"] = ["Huancayo"]
         node1 = Node("Tumbes")
         node2 = Node("Trujilo")
         node3 = Node("Iquitos")
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Implementación de algoritmos de búsqueda BFS y DFS
 
 **BFS** = BreadthFirstSearch
+
 **DFS** = DepthFirstSearch
