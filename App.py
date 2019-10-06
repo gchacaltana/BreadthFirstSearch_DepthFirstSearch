a63457a895611748ccd0d9fa656133b85e44e72a
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-10-05 22:40:30 -0500

    add main function App Class

--- App.py
+++ App.py
@@ -1,57 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # App Script
-
 __author__ = "Gonzalo Chacaltana Buleje"
 __email__ = "gchacaltanab@gmail.com"
 
+import json
 from Node import Node
 
-class App(object):
-    listNodes = []
-    parentNode = {}
-    def __init__(self, nodes):
-        self.nodes = nodes
-        self.createNodes()
 
-    def createNodes(self):
+class App(object):
+
+    def __init__(self):
+        self.listNodes = []
+        self.parentNode = {}
+
+    def main(self):
+        with open("routes.json", "r") as f:
+            routes = json.load(f)
+        self.createNodes(routes)
+        self.displayNodes()
+
+    def createNodes(self, routes):
         i = 0
-        for n in self.nodes:
-            i +=1
+        for n in routes:
+            i += 1
             self.parentNode[i] = Node(n)
-            #print(i)
-            #print([c for c in self.nodes[n]])
-            #print("-----------")
-            for cn in self.nodes[n]:
-                childNode = None
+            for cn in routes[n]:
                 childNode = Node(cn)
                 self.parentNode[i].addChild(childNode)
-                if cn not in [node.name for node in self.listNodes]:
-                    self.listNodes.append(childNode)
-            if n not in [node.name for node in self.listNodes]:
-                self.listNodes.append(self.parentNode[i])
+            self.listNodes.append(self.parentNode[i])
 
     def displayNodes(self):
         print([n.name for n in self.listNodes])
 
+
 if __name__ == "__main__":
-    nodes = {}
-    nodes["Tumbes"] = ["Trujillo", "Moyobamba", "Iquitos"]
-    nodes["Trujillo"] = ["Lima", "Huancayo"]
-    nodes["Lima"] = ["Ica", "Ayacucho"]
-    nodes["Ica"] = ["Arequipa"]
-    nodes["Arequipa"] = ["Moquegua"]
-    nodes["Moyobamba"] = ["Huancayo"]
-    nodes["Huancayo"] = ["Ayacucho", "Cusco"]
-    nodes["Ayacucho"] = ["Abancay", "Ica"]
-    nodes["Cusco"] = ["Puno", "Arequipa"]
-    nodes["Puno"] = ["Moquegua"]
-    nodes["Moquegua"] = ["Tacna"]
-    nodes["Iquitos"] = ["Pucallpa"]
-    nodes["Pucallpa"] = ["Huancayo"]
-    app = App(nodes)
-    #app.displayNodes()
-    for i in app.parentNode:
-        print("Nodo Padre: ", app.parentNode[i].name)
-        print("Nodos Hijos: ", app.parentNode[i].getChildrenNodes())
-    
+    app = App()
+    app.main()
