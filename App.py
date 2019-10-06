c5e7575c0a404b63e802314e13e7b80ac22c76ba
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-10-06 01:32:43 -0500

    reorder source in functions

--- App.py
+++ App.py
@@ -11,42 +11,48 @@ from BFS import BFS
 
 class App(object):
 
-    def __init__(self):
+    def __init__(self, jsonfile, searched):
         self.listNodes = []
         self.parentNode = {}
+        self.jsonfile = jsonfile
+        self.searched = searched
 
-    def main(self, jsonfile):
-        with open(jsonfile, "r") as f:
-            routes = json.load(f)
-        self.createNodes(routes)
+    def main(self):
+        self.openFile()
+        self.createNodes()
         self.displayNodes()
         self.breadthFirstSearch()
 
-    def createNodes(self, routes):
+    def openFile(self):
+        print("\n1. Abriendo fichero")
+        with open(self.jsonfile, "r") as f:
+            self.routes = json.load(f)
+
+    def createNodes(self):
         i = 0
-        print("\nCargando los nodos")
-        for n in routes:
+        print("\n2. Cargando nodos")
+        for n in self.routes:
             i += 1
             self.parentNode[i] = Node(n)
-            for cn in routes[n]:
+            for cn in self.routes[n]:
                 childNode = Node(cn)
                 self.parentNode[i].addChild(childNode)
             self.listNodes.append(self.parentNode[i])
 
     def displayNodes(self):
-        print("Lista de Nodos")
+        print("\n3. Nodos cargados")
         print([n.name for n in self.listNodes])
 
     def breadthFirstSearch(self):
         print("\nIniciando Busqueda Breadth First Search")
-        print("------------------------------")
-        bsf = BFS(self.listNodes, "Arequipa")
+        print("------------------------------------------")
+        bsf = BFS(self.listNodes, self.searched)
         bsf.search()
 
 
 if __name__ == "__main__":
     try:
-        app = App()
-        app.main("routes.json")
+        app = App("routes.json", "Arequipa")
+        app.main()
     except (ValueError, FileNotFoundError, AttributeError) as ex:
         print(ex)
