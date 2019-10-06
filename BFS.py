dac35c589a99e70c5f4ac2be637a50807f25c3d3
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-10-06 01:53:08 -0500

    change source - add extends Uninformed Search Algorithm

--- BFS.py
+++ BFS.py
@@ -1,43 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Breadth First Search Algorithm
 __author__ = "Gonzalo Chacaltana Buleje"
 __email__ = "gchacaltanab@gmail.com"
 
 from Node import Node
+from UninformedSearchAlgorithm import UnniformedSearchAlgorithm
 
 
-class BFS(object):
+class BFS(UnniformedSearchAlgorithm):
     def __init__(self, listNodes, searched):
-        self.listNodes = listNodes
-        self.searched = searched
-        self.createQueue()
-
-    def createQueue(self):
-        self.queue = []
-        self.queue.append(self.listNodes[0])
+        UnniformedSearchAlgorithm.__init__(self, listNodes, searched)
 
     def addQueue(self, node):
         if node.name not in [n.name for n in self.queue]:
             self.queue.append(node)
-
-    def readQueue(self):
-        return self.queue.pop(0)
-
-    def getQueueLen(self):
-        return len(self.queue)
-
-    def getNodeFromList(self, name):
-        for node in self.listNodes:
-            if node.name == name:
-                return node
-
-    def validateLenQueue(self):
-        if self.getQueueLen() == 0:
-            raise Exception("La cola esta vacia")
-
-    def matchSearched(self, nodeName):
-        if nodeName == self.searched:
-            raise Exception("Ciudad encontrada: %s" % nodeName)
 
     def search(self):
         try:
@@ -54,10 +31,3 @@ class BFS(object):
                 print("Queue: ", [n.name for n in self.queue])
         except (Exception) as error:
             print(error)
-
-    def insertNodeChildQueue(self, node):
-        childrenNodes = node.getChildrenNodes()
-        for child in childrenNodes:
-            childNode = self.getNodeFromList(child.name)
-            if (isinstance(childNode, Node)):
-                self.addQueue(childNode)
